4cfa94820f887b9737c45f40eb24fe001995c3cb
sharon <sharonoptsout@gmail.com>
2018-10-11 20:29:14 +0000

    automatically backed up by learn

---
+++ lib/author.rb
@@ -0,0 +1,9 @@
+class author
+
+attr_accessor :name
+
+def initialize(name)
+  @name = name
+end
+
+end
--- lib/post.rb
+++ lib/post.rb
@@ -1,6 +1,5 @@
 class Post
   attr_accessor :title, :author
-
   def initialize(title)
     @title = title
   end
